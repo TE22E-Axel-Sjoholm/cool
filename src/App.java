674d9729d81353ef32be741d745cee466caa48d8
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2023-09-11 08:54:26 +0200

    importerade scanner och skapade variabel

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {;
+        Scanner tangentbord = new Scanner(System.in);
     }
 }
