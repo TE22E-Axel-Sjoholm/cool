d0d351aaa5eb99fb8c8f80d189a90d4b1f0f1ea5
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2023-09-11 08:57:45 +0200

    la till text och int för tal1 o 2

--- src/App.java
+++ src/App.java
@@ -2,5 +2,10 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {;
         Scanner tangentbord = new Scanner(System.in);
+        System.out.println("Divisions program:");
+        System.out.println("Ange tal 1:");
+        int tal1 = tangentbord.nextInt();
+        System.out.println("Ange tal 2:");
+        int tal2 = tangentbord.nextInt();
     }
 }
