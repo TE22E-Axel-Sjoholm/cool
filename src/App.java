86ac9277841a53ce3841c2dd8866931a6fbf525f
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2023-09-11 09:09:18 +0200

    la till kvot för double och int. printar svar

--- src/App.java
+++ src/App.java
@@ -3,9 +3,15 @@ public class App {
     public static void main(String[] args) throws Exception {;
         Scanner tangentbord = new Scanner(System.in);
         System.out.println("Divisions program:");
-        System.out.println("Ange tal 1:");
+        System.out.print("Ange tal 1:");
         int tal1 = tangentbord.nextInt();
-        System.out.println("Ange tal 2:");
+        System.out.print("Ange tal 2:");
         int tal2 = tangentbord.nextInt();
-    }
+        double DbKvot = (double) tal1 / tal2;
+        int intKvot = tal1 / tal2;
+        System.out.println("Kvoten Är: "+DbKvot);
+        System.out.println("Kvoten Är: "+intKvot);
+
+        
+    }  
 }
